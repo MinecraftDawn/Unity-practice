8bf59372d4241ce94d46a89b8d3427ba23ec8c4a
MinecraftDawn <eric4902077@gmail.com>
2023-11-20 08:17:41 +0800

    增加背景/角色需要在地面上才能跳躍

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class Player : MonoBehaviour
     public float speed_y = 0.1f;
     public float max_speed_x = 1f;
     public float max_speed_y = 1f;
+    public bool isOnFloor = false;
     Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
@@ -21,14 +23,13 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector3 vector = new Vector3(0,0); 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && isOnFloor)
         {
             this.rigidbody.AddForce(new Vector2(0, speed_y), ForceMode2D.Impulse);
             if (rigidbody.velocity.y > max_speed_y) rigidbody.velocity = new Vector2(rigidbody.velocity.x, max_speed_y);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            
             this.rigidbody.AddForce(new Vector2(speed_x, 0), ForceMode2D.Impulse);
             if (rigidbody.velocity.x > max_speed_x) rigidbody.velocity = new Vector2(max_speed_x, rigidbody.velocity.y);
         }
@@ -40,5 +41,22 @@ public class Player : MonoBehaviour
         
 
         
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isOnFloor = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isOnFloor = false;
+        }
     }
 }
